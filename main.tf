2d644c45f4783811d26c356428e4732b2d0188dd
ilyas <cyberilyas@gmail.com>
2024-07-29 20:51:15 +0100

    update

--- main.tf
+++ main.tf
@@ -4,3 +4,11 @@ provider "aws" {
 resource "aws_s3_bucket" "zephrya00" {
   bucket = "zephrya00"
 }
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "zephrya00"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
